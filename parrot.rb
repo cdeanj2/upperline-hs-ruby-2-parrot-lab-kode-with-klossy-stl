fc1baaa3b0ecb40bea3d522fe24d06559d8faff8
Caroline Jordan <cdeanj2@gmail.com>
2016-06-21 18:59:07 +0000

    Finished parrot lab. Friendly parrot needs to die! :(

--- parrot.rb
+++ parrot.rb
@@ -2,25 +2,32 @@
 def happy_parrot(thing)
   "I am so happy about #{thing}!"
 end
-
+puts happy_parrot("KodingWithKlossy").upcase
 # Boring Parrot - Write a method for a boring parrot that just returns whatever string you give him as an argument.
+def boring_parrot(string)
+  "#{string}"
+end
+puts boring_parrot("I'm so boring!!!!").upcase
 
-
-
-# Math Parrot - Create a method that accepts two numbers as arguments and adds them together! 
-
-
-
+# Math Parrot - Create a method that accepts two numbers as arguments and adds them together!
+def math_parrot(number1,number2)
+  number1 + number2
+end
+puts math_parrot(2,3)
 # Friendly Parrot - This parrot greets people by returning their name and age (don't forget to pass that information in as arguments).
-
-
-
+def friendly_parrot(name, age)
+  "Hi #{name}, You are #{age} years old!"
+end
+puts friendly_parrot("Anna", 16).upcase
 # Favorites Parrot - Tell this parrot about your three favorite things and he will return "I love <that thing> too!" for each of them.
-
+def favorite_parrot(fav1, fav2, fav3)
+  "I love #{fav1} too! I love #{fav2} too! I love #{fav3} too!"
+end
+puts favorite_parrot("Karlie Kloss", "Coding", "Cookies").upcase
 
 
 # Now try calling your methods below with any arguments of your choice and puts them to the screen. Like this:
-puts happy_parrot("waffles")
+
 # call your methods here
 
 
@@ -30,6 +37,3 @@ puts happy_parrot("waffles")
 
 
 # The spell has been broken and everyone is speaking normally again. The parrots are really excited about it though - make them shout in all caps.
-
-
-
